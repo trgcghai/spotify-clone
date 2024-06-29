bc0947a75e1c04cdb6a1afce73e0e1794226e577
trgcghai <conghai.tpma@gmail.com>
2024-06-29 20:46:10 +0700

    do function bar

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,17 +21,7 @@ export default function RootLayout({ children }) {
       >
         <div className={`${style.layout} gap-2`}>
           <SideNav></SideNav>
-          <div className="col-start-7 col-span-full bg-darkCoal rounded-md" style={{ height: '852px' }}>
-            <div className='p-4 h-full relative'>
-              <div className="absolute top-0 right-0 left-0 p-4">
-                <Header></Header>
-              </div>
-              <div className={`h-full mt-28 overflow-y-scroll ${style.scrollbar}`}>
-                {children}
-                <Footer></Footer>
-              </div>
-            </div>
-          </div>
+          {children}
           <div className=" text-white" style={{ gridColumnStart: 17 }}>fdsa</div>
         </div>
         <div className="fixed z-10 bg-black bottom-0 right-0 left-0 p-2">
